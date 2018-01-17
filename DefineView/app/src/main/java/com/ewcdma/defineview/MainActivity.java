9356ee76a5388b6b53555ff31af991a6ae6e60a3
jason_luo <jason@huahuico.cm>
2018-01-17 15:24:24 +0800

    22

--- DefineView/app/src/main/java/com/ewcdma/defineview/MainActivity.java
+++ DefineView/app/src/main/java/com/ewcdma/defineview/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e(TAG, "onCreate: " );
+        Log.e(TAG, "onCreate: 22" );
         
     }
 }
